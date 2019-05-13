12e5d740361363f3090a572846ce434fc02676ea
bilal çalışkan <bilal_caliskan_58@hotmail.com>
2019-05-13 21:44:05 +0300

    Template Giydirildi Demo İçeriğe Dokunulmadı Header Linkleri Düzeltildi

---
+++ Iskuriyer/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Iskuriyer.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult UserDash()
+        {
+            return View();
+        }
+
+        public ActionResult BusinessProfile()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
